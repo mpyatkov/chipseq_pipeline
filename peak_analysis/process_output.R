00feeec3a45aaba8460af61cbc3d2f938963a314
mpyatkov <test@test.com>
2023-02-06 16:49:34 -0500

    Edited scripts for peak analysis

--- peak_analysis/process_output.R
+++ peak_analysis/process_output.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(writexl)
 
-bed <- read_tsv("~/tmp/mnt/max/G196/peak_analysis/combined.output", col_names = F) %>% 
+bed <- read_tsv("combined.output", col_names = F) %>% 
     select(chr = 1, start = 2, end = 3, who = 4) %>%
     distinct() %>% 
     group_by(chr, start, end) %>% 
@@ -11,5 +11,4 @@ bed <- read_tsv("~/tmp/mnt/max/G196/peak_analysis/combined.output", col_names = 
     select(chr, start, end, samples_intersects = who, how_many_samples_intersects = n) %>% 
     arrange(desc(how_many_samples_intersects))
 
-#write_xlsx(bed,'~/tmp/mnt/max/G196/peak_analysis/peaks_intersections.xlsx')
 write_xlsx(bed,'peaks_intersections.xlsx')
--- peak_analysis/run.sh
+++ peak_analysis/run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+## Find overlapping peaks 
+
 module load bedtools
-module load R
+module load R/4.1.2
 
-input=combined.sorted.merged.bed
+find ../G* -name "*narrowPeak*bed" | grep -v Scripts | grep -v blacklist | grep -v StrgtPksRm | xargs -n1 -I{} cp {} ./
 
+## combine all narrowPeaks inside current directory in one file
+cat *.bed > combined.bed
+
+sort -k1,1 -k2,2n combined.bed > combined.sorted.bed
+bedtools merge -i combined.sorted.bed > combined.sorted.merged.bed
+
+input_files=`find . -name "*narrowPeak.bed" | xargs -n1 basename |sort | paste -s -d " "`
+input_names=`find . -name "*narrowPeak.bed" | xargs -n1 basename |sort | grep -E -o 'M[0-9]+' | paste -s -d " "`
 bedtools intersect -wa -wb\
-    -a ${input} \
-    -b G196_M9_MACS2_peaks.narrowPeak.bed G196_M10_MACS2_peaks.narrowPeak.bed G196_M11_MACS2_peaks.narrowPeak.bed G196_M12_MACS2_peaks.narrowPeak.bed G196_M13_MACS2_peaks.narrowPeak.bed G196_M14_MACS2_peaks.narrowPeak.bed \
+    -a combined.sorted.merged.bed \
+    -b ${input_files} \
     -sorted \
-    -names M9 M10 M11 M12 M13 M14 > combined.output
+    -names ${input_names} > combined.output
 
 ## just aggregate and create xls file
 ## hardcoded combined.output as input
@@ -17,4 +27,3 @@ Rscript ./process_output.R
 
 ## almost work (does not remove duplicates)
 # cat combined.output | bedtools groupby -i combined.output -g 1-3 -c 4 -o collapse,count > combined.output1
-
