d99f1b0246f21106adbda36ee5bf698d226a08a0
mpyatkov <test@test.com>
2023-02-14 15:21:28 -0500

    script changes extension and check read names for R1 and R2 suffixes

--- copy_samples.py
+++ copy_samples.py
@@ -8,6 +8,8 @@ from pathlib import Path
 import sys
 import os
 
+RIGHT_EXTENSION = ".fastq.gz"
+
 ## just for debug
 # prelim_path = '/net/waxman-server/mnt/data/volume2/Waxman_Illumina_HiSeq_Raw_Data_02/G197/usftp21.novogene.com/01.RawData'
 # prelim_path = '/net/waxman-server/mnt/data/volume2/Waxman_Illumina_HiSeq_Raw_Data_02/G196/usftp21.novogene.com/01.RawData'
@@ -20,7 +22,7 @@ def hamming_distance(str1, str2):
 ## calculate position of disimilarities
 ## I use this function to get the common part of the name and drop
 ## tail with extension and read information (need for lane dialog with user)
-def hamming_index(str1,str2):
+def hamming_indexes(str1,str2):
     ix = 0
     indexes = []
     for c1,c2 in zip(str1, str2):
@@ -40,7 +42,7 @@ def transform_samples_dict(key):
             str2 = str(str2)
             
             if hamming_distance(str1, str2) == 1:
-                hamming_ix = hamming_index(str1,str2)[0]
+                hamming_ix = hamming_indexes(str1,str2)[0]
 
                 ## chop _1.gz -> convert to path -> extract name -> use as a key
                 hamming_prefix = Path(str1[:hamming_ix-1]).name
@@ -51,14 +53,42 @@ def transform_samples_dict(key):
     
     return transformed_dict
 
-def create_symlink(files, dst):
+def add_before_position(s, ix, val):
+    return s[0:ix]+val+s[ix:] 
+
+def create_symlink(fq_files, dst):
+
+    files = fq_files
+
+    
+    ## set flag if it is require to change indexes _1,_2 -> R1,R2
+    change_read_name = False
+    f1,f2 = str(Path(files[0]).name),str(Path(files[1]).name)
+    ix = hamming_indexes(f1,f2)[-1]
+    
+    if f1[ix-1] != "R":
+        change_read_name = True
+        
+    
     for fastq_file in files:
         file_src = Path(fastq_file)
         fname = file_src.name
-        
+
+        ## change extension from fq.gz etc... to fastq.gz
+        ## if it required
+        extension = "".join(file_src.suffixes)
+        if extension != RIGHT_EXTENSION:
+            fname = fname.replace(extension, RIGHT_EXTENSION)
+
+        ## change _1,_2 -> _R1,_R2
+        ## if it is required
+        if change_read_name:
+            fname = add_before_position(fname,ix,"R")
+           
         file_dst = Path(dst+'/'+fname)
         
-        if not file_dst.is_file():
+        # if not file_dst.is_file():
+        if not file_dst.is_symlink():
             os.symlink(fastq_file, file_dst)
 
 def create_dir_and_make_links(samples, sample_id, lane_index):
@@ -81,7 +111,7 @@ def user_dialog_samples(sample_id):
     res = False
     while True:
         
-        print(f"\nWould you like to copy/make link for {sample_id} (YyNn, default: N): ) ", end='')
+        print(f"\nWould you like to copy/make link for {sample_id} (YyNn, default: N): ", end='')
         answer = input()
         if answer not in "Yynn":
             print("Wrong answer. Try it again")
