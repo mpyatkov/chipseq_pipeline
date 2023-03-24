14b87250976cf4f338f46b3618b7f40829c63cbd
mpyatkov <test@test.com>
2023-03-24 14:10:56 -0400

    Added extradetailed report for peak analysis

--- peak_analysis/process_output_extradetailed.R
+++ peak_analysis/process_output_extradetailed.R
@@ -34,7 +34,39 @@ ParseArguments <- function() {
 argv <- ParseArguments()
 
 sample_labels <- read_tsv(argv$sample_labels, col_names=T) %>% 
-  select(who = Sample_ID, desc = Description)
+  select(who = Sample_ID, desc = Description) %>% 
+  mutate(ix = str_pad(row_number(), width = 2, pad = "0"),
+         desc = str_glue("{ix}_{who}\n{desc}")) %>% 
+  select(-ix)
+  
+xls <- map_dfr(list.files(pattern = "*peaks.xls"), function (xls_fname){
+  # fname <- str_extract(xls_fname,"^G\\d+\\w+\\d+(?=_MACS)")
+  tmp <- read_tsv(xls_fname, comment = "#", col_names = T) %>% 
+    select(peak_id = 10, length = 4, pileup = 6, fold_enrichment = 8, log10_qvalue = 9)
+})
+
+extra_details <- left_join(read_tsv("combined.output", col_names = F), xls, by = c("X8" = "peak_id")) %>% 
+  left_join(., sample_labels, by=c("X4" = "who")) %>% 
+  mutate(X4 = desc) %>% select(-desc) %>% 
+  # group_by(X1, X2, X3, X4) %>% 
+  # mutate(max_param = max(fold_enrichment), n = n()) %>%
+  # arrange(desc(max_param), .by_group = T) %>% 
+  # ungroup() %>% 
+  group_by(X1, X2, X3, X4) %>% 
+  # mutate(rowid = row_number()) %>% 
+  ## because sometimes enrichment can be the same for two peaks
+  ## we need to extract only first
+  filter(fold_enrichment == max(fold_enrichment) & pileup == max(pileup) & length == max(length)) %>% 
+  # select(-max_param, -rowid) %>% 
+  ungroup() %>% 
+  select(-X5,-X6,-X7, -X8) %>%
+  # group_by(X1, X2, X3) %>% mutate(how_many_samples_intersect = n()) %>% ungroup() %>% 
+  pivot_longer(c("length", "pileup", "fold_enrichment", "log10_qvalue"), values_to = "param") %>% 
+  # arrange(X4) %>% 
+  pivot_wider(names_from = c("X4","name"), values_from = param, names_sort = T)
+
+##### PARTIALY WORKING
+
 
 bed <- read_tsv("combined.output", col_names = F) %>% 
   select(chr = 1, start = 2, end = 3, who = 4) %>%
@@ -43,20 +75,30 @@ bed <- read_tsv("combined.output", col_names = F) %>%
   mutate(how_many_samples_intersect = n()) %>%
   #filter(how_many_samples_intersect > 1) %>% 
   ungroup() 
-  
+
 bed <- left_join(bed, sample_labels) %>% 
-  mutate(who = str_glue("{who}\n{desc}")) %>% 
-  select(-desc)
+  #mutate(desc = str_glue("{who}\n{desc}")) %>% 
+  select(-who)
 
 bed <- bed %>%  
   mutate(fake_value = 1) %>% 
-  pivot_wider(names_from = who, values_from = fake_value) %>% 
+  pivot_wider(names_from = desc, values_from = fake_value, names_sort = T, values_fill = 0) %>% 
   mutate(ucsc_line = str_glue("{chr}:{start}-{end}")) %>% 
   relocate(ucsc_line,.before = chr) %>% 
-  mutate_all(~replace(., is.na(.), 0)) %>% 
+  #mutate_all(~replace(., is.na(.), 0)) %>% 
   arrange(desc(how_many_samples_intersect))
 
-write_xlsx(bed,'peaks_intersections_detailed.xlsx')
+# extra_details <- left_join(bed,xls, by = c("X8" = "peak_id")) %>% 
+#   group_by(X1, X2, X3,X4) %>% 
+#   mutate(max_param = max(fold_enrichment)) %>% filter(fold_enrichment == max_param) %>% select(-max_param) %>% ungroup() %>% 
+#   select(-X5,-X6,-X7, -X8) %>%
+#   group_by(X1, X2, X3) %>% mutate(how_many_samples_intersect = n()) %>% ungroup() %>% 
+#   ungroup %>% 
+#   pivot_longer(c("length", "pileup", "fold_enrichment", "log10_qvalue"), values_to = "param") %>% 
+#   pivot_wider(names_from = c("X4","name"), values_from = param)
+
+result <- left_join(bed, extra_details, by=c("chr"= "X1","start" = "X2","end" = "X3"))
+write_xlsx(result,'peaks_intersections_extradetailed.xlsx')
 
 ## create xlsx workbook
 # library(openxlsx)
--- peak_analysis/run.sh
+++ peak_analysis/run.sh
@@ -6,11 +6,15 @@ set -eu
 module load bedtools
 module load R
 
-echo "Clean all bed files inside current directory"
-rm -rf *.bed
+# echo "Clean all bed files inside current directory"
+# rm -rf *.bed
+rm -rf combined.* *.xlsx
 
-echo "Copying files"
+# echo "Copying files"
 find ../G* -name "*narrowPeak*bed" | grep -v Scripts | grep -v blacklist | grep -v StrgtPksRm | xargs -n1 -I{} cp {} ./
+
+# echo "Copying xls"
+find ../G* -name "*xls" | grep -v Scripts | grep -v blacklist | grep -v StrgtPksRm | xargs -n1 -I{} cp {} ./
 
 echo "Create combined.bed"
 ## combine all narrowPeaks inside current directory in one file
@@ -23,13 +27,13 @@ echo "Merge combined.sorted.bed to combined.sorted.merged.bed"
 bedtools merge -i combined.sorted.bed > combined.sorted.merged.bed
 
 echo "Make intersection of all bed files with combined.sorted.merged.bed"
-input_files=`find . -name "*narrowPeak.bed" | xargs -n1 basename |sort | paste -s -d " "`
+input_files=`find . -maxdepth 1 -name "*narrowPeak.bed" | xargs -n1 basename |sort | paste -s -d " "`
 
 ## only M numbers. ex. M9,M10,... (convinient when need short names)
 # input_names=`find . -name "*narrowPeak.bed" | xargs -n1 basename |sort | grep -E -o 'M[0-9]+' | paste -s -d " "`
 
 ## GM numbers. Ex. G196_M9, G196_M10 (more convinient if Sample_Labels.txt will be parsed)
-input_names=`find . -name "*narrowPeak.bed" | xargs -n1 basename |sort | grep -E -o 'G[0-9]+_?M[0-9]+' | paste -s -d " "`
+input_names=`find . -maxdepth 1 -name "*narrowPeak.bed" | xargs -n1 basename |sort | grep -E -o 'G[0-9]+_?M[0-9]+M?[0-9]+?' | paste -s -d " "`
 
 bedtools intersect -wa -wb\
     -a combined.sorted.merged.bed \
@@ -40,7 +44,12 @@ bedtools intersect -wa -wb\
 ## just aggregate and create xls file
 ## hardcoded combined.output as input
 echo "Run process_output.R"
-Rscript ./process_output_detailed.R
 Rscript ./process_output_compact.R
+
+echo "Run process_output_detailed.R"
+Rscript ./process_output_detailed.R
+
+echo "Run process_output_extradetailed.R"
+Rscript ./process_output_extradetailed.R
 ## almost work (does not remove duplicates)
 # cat combined.output | bedtools groupby -i combined.output -g 1-3 -c 4 -o collapse,count > combined.output1
