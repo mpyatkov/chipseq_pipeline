4c142144b0030bfbc33bc3a690e6d94764ced6cc
mpyatkov <test@test.com>
2023-10-11 16:21:19 -0400

    Added configuration file for diffreps

---
+++ Scripts/00_Setup_Pipeline/diffreps_parser.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eu
+
+config="./diffreps_config.txt"
+
+while IFS=';' read -r num treatment_name control_name treatment_samples control_samples norm diffreps_window; do
+    echo $num
+    echo $treatment_name
+    echo $control_name
+    echo $treatment_samples
+    echo $control_samples
+    echo $norm
+    echo $diffreps_window
+    
+    template_name="10_diffreps_${treatment_name}_vs_${control_name}_${norm}_${num}"
+    rm -rf "../${template_name}"
+    
+    cp -a ../TEMPLATE_10_diffreps "../${template_name}"
+
+    pushd "../${template_name}"
+
+    sed -i "s/TEMPLATE_CONTROL_NAME/${control_name}/g" Run_Jobs.sh
+    sed -i "s/TEMPLATE_TREATMENT_NAME/${treatment_name}/g" Run_Jobs.sh
+    sed -i "s/TEMPLATE_COMPARISON_NUMBER/${num}/g" Run_Jobs.sh
+    sed -i "s/TEMPLATE_DIFFREPS_WINDOW/${diffreps_window}/g" Run_Jobs.sh
+    sed -i "s/TEMPLATE_NORMALIZATION/\"${norm}\"/g" Run_Jobs.sh
+    
+    ctrl_samples=`echo "${control_samples}" | sed -e 's/,/|/g'`
+    treatment_samples=`echo "${treatment_samples}" | sed -e 's/,/|/g'`
+    sed -i "s/TEMPLATE_CONTROL_SAMPLES/\"${ctrl_samples}\"/g" Run_Jobs.sh
+    sed -i "s/TEMPLATE_TREATMENT_SAMPLES/\"${treatment_samples}\"/g" Run_Jobs.sh
+    
+    popd
+    echo "---"
+done < <(cat $config | tr -d " " | sed -e '/^#/d')
+
+
+    # for control in $(echo $control_samples | tr "," "\n"); do
+    #     echo "control: $control"
+    # done
+
+    # for treatment in $(echo $treatment_samples | tr "," "\n"); do
+    #     echo "treatment: $treatment"
+    # done
+    
+
+
+# copy directory with different name
+# 10_diffreps_Treatment_vs_Control_DIFFREPS_1
+# 10_diffreps_Treatment_vs_Control_MACS2_2
+
+# go inside
+# replace TEMPLATE variables to the real one in Run_Jobs.sh
+# replace Control_Samples.txt
+# replace Treatment_Samples.txt
+# create norm file if normalization is MACS2
+# go back
---
+++ Scripts/TEMPLATE_10_diffreps/Run_Jobs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eu
+
+rm -rf *.o* *.po*
+
+source ../00_Setup_Pipeline/01_Pipeline_Setup.sh
+
+Control_Samples_NAME=TEMPLATE_CONTROL_NAME
+Treatment_Samples_NAME=TEMPLATE_TREATMENT_NAME
+COMPAR_NUM=TEMPLATE_COMPARISON_NUMBER
+WINDOW_SIZE=TEMPLATE_DIFFREPS_WINDOW
+NORMALIZATION=TEMPLATE_NORMALIZATION
+FRAG_SIZE=0
+
+CONTROL_SAMPLES=TEMPLATE_CONTROL_SAMPLES
+TREATMENT_SAMPLES=TEMPLATE_TREATMENT_SAMPLES
+
+## create control and treatment samples files
+cat ../00_Setup_Pipeline/Sample_Labels.txt | grep -E "Sample_DIR|${CONTROL_SAMPLES}" > Control_Samples.txt
+cat ../00_Setup_Pipeline/Sample_Labels.txt | grep -E "Sample_DIR|${TREATMENT_SAMPLES}" > Treatment_Samples.txt
+
+## if normalization is MACS2 than norm.txt should be present in this
+## directory
+
+if [ "$NORMALIZATION" = "RIPPM" ]; then
+    
+    #Extract normalization factors and represent them in the 1/factor
+    # form, because diffreps works only with such representation
+    
+    control_norm_line=$(cat ../09_RiPPM_Normalization/Job_Summary/Norm_Factors.txt | \
+	grep -v FRAG | \
+	cut -f1,4 | \
+	awk '{printf "%s %.2f\n", $1, 1/$2}' | \
+	grep -E "${CONTROL_SAMPLES}" | \
+	cut -d " " -f2 | \
+	paste -s -d " ")
+
+    treatment_norm_line=$(cat ../09_RiPPM_Normalization/Job_Summary/Norm_Factors.txt | \
+	grep -v FRAG | \
+	cut -f1,4 | \
+	awk '{printf "%s %.2f\n", $1, 1/$2}' | \
+	grep -E "${TREATMENT_SAMPLES}" | \
+	cut -d " " -f2 | \
+	paste -s -d " ")
+
+    echo "treatment ${treatment_norm_line}" > norm.txt
+    echo "control ${control_norm_line}" >> norm.txt
+fi
+
+DIR_name=`basename ${SCRIPT_DIR}`
+Step_Num=$(echo ${DIR_name} | cut -d'_' -f 1)
+Job_Name="Step_'${Step_Num}_${Dataset_Label}"
+
+(set -x; qsub -N ${Job_Name}'_diffReps_'${COMPAR_NUM} -P wax-es -q !linga diffReps.qsub ${Dataset_DIR} ${Dataset_Label} ${Control_Samples_NAME} ${Treatment_Samples_NAME} ${COMPAR_NUM} ${WINDOW_SIZE} ${FRAG_SIZE} ${Input_Sites_RiPPM} ${SCRIPT_DIR})
