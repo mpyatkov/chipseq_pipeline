5b1fe79869faddaa4be8d7050d835e20c51d5868
mpyatkov <test@test.com>
2024-01-12 14:35:48 -0500

    Added step 11, summary from diffReps
    1. Aggregating PDFs by groups
    2. Aggregated XLSX report
    3. Creating bigBed tracks and track lines

---
+++ Scripts/11_DiffReps_Summary/Job_Scripts/diffreps_output_parser.R
@@ -0,0 +1,129 @@
+library(argparser)
+
+ParseArguments <- function() {
+    p <- arg_parser('Parsing diffReps output')
+    p <- add_argument(p,'--pattern', default="diffReps_", help="search pattern for diffReps output files")
+    p <- add_argument(p, '--path', default=".", help="path to the dir which contains diffReps output directories")
+    p <- add_argument(p, '--output', default="Summary_normalization_factors.xlsx", help="output filename")
+    return(parse_args(p))
+}
+
+argv <- ParseArguments()
+
+library(stringr)
+library(purrr)
+library(readr)
+library(dplyr)
+library(tidyr)
+
+files <- list.files(path = argv$path, pattern = argv$pattern, recursive = T, full.names = T) %>% 
+    keep(function(x){str_detect(x,"\\.vs\\.") && str_detect(x,"annotated|hotspot", negate = T)})
+
+## What we need to parse (diffReps output)
+# Treatment files: G220M03_fragments.bed        G220M04_fragments.bed
+# Control files: G220M05_fragments.bed  G220M06_fragments.bed
+# Window size: 1000
+# Treatment normalization constant: 2.23        0.28
+# Control normalization constant: 0.12  1.37
+
+extract_tab_separated <- function(header, tag) {
+    keep(header, function(line) {str_detect(line, tag)}) %>% 
+        str_split(":", simplify = T) %>% 
+        str_trim() %>% 
+        str_split(pattern = "\t") %>% .[[2]]
+}
+
+parse_diffreps_file <- function(f, header) {
+  normalization_caller <- ifelse(str_detect(f,"RIPPM"), "RIPPM","DIFFREPS")
+  conditions <- basename(f) %>% str_replace("diffReps_","") %>% str_split(".vs.", simplify = T)
+  cond_tr <- conditions[1]
+  cond_ctrl <- conditions[2]
+  tr_files <- extract_tab_separated(header, "Treatment files") %>% str_replace("_fragments.bed", "")
+  ctrl_files <- extract_tab_separated(header, "Control files") %>% str_replace("_fragments.bed", "")
+  window <- extract_tab_separated(header, "Window size")
+  norm_tr <- extract_tab_separated(header, "Treatment normalization constant")
+  norm_ctrl <- extract_tab_separated(header, "Control normalization constant")
+  
+  tibble(
+      fname = basename(f),
+      group = c(rep("TREATMENT", length(tr_files)), rep("CONTROL", length(ctrl_files))),
+      condition = c(rep(cond_tr, length(tr_files)), rep(cond_ctrl, length(ctrl_files))),
+      sample_id = c(tr_files, ctrl_files),
+      normfactors = as.numeric(c(norm_tr, norm_ctrl)),
+      window = as.numeric(window),
+      norm_caller = normalization_caller
+  )
+}
+
+tst <- map_dfr(files, function(f){
+    header <- read_lines(f, n_max = 32)
+    parse_diffreps_file(f, header)
+})
+
+l1 <- tst %>% 
+    mutate(window = str_glue("{norm_caller}_{window}"),
+           sample_id = str_glue("{sample_id}_{group}"),
+           group = str_glue("{condition}_{group}")) %>% 
+    select(-norm_caller, -condition) %>% 
+    group_by(fname) %>% 
+    group_split()
+
+lgroup_to_ltables <- function(df) {
+    norm_factors_table <- df %>% pivot_wider(names_from = window, values_from = normfactors)
+
+    fname <- norm_factors_table %>% select(fname) %>% distinct() %>% pull(fname) %>% 
+        str_replace_all(., "\\.", "_")
+    
+    nf <- norm_factors_table %>% 
+        select(-fname, -group)
+    
+    ## average by factors only
+    all <- nf %>% 
+        summarise_at(vars(starts_with("RIPPM") | starts_with("DIFFREPS")), list(~round(mean(.), 2))) %>% 
+        mutate(sample_id = "all") %>% 
+        select(sample_id, everything())
+    
+    ## average by groups/factors
+    by_groups <- norm_factors_table %>% 
+        group_by(group) %>% 
+        summarise_at(vars(starts_with("RIPPM") | starts_with("DIFFREPS")), list(~round(mean(.), 2))) %>% 
+        mutate(group = str_glue("Group_{group}")) %>% 
+        select(sample_id = group, everything())
+
+    ratio_treat_ctrl <- by_groups %>% 
+        pivot_longer(starts_with("RIPPM") | starts_with("DIFFREPS"), names_to = "factor", values_to = "values") %>% 
+        mutate(sample_id = ifelse(str_detect(sample_id, "TREATMENT"), "TREATMENT", "CONTROL")) %>% 
+        pivot_wider(names_from = sample_id, values_from = values) %>% 
+        rowwise() %>% 
+        mutate(TR_CTRL = round(TREATMENT/CONTROL,2)) %>% 
+        select(-TREATMENT,-CONTROL) %>% 
+        pivot_wider(names_from = factor, values_from = TR_CTRL) %>% 
+        mutate(sample_id = "Treatment/Control ratio") %>% 
+        select(sample_id, everything())
+    
+    res <- bind_rows(nf, all, by_groups, ratio_treat_ctrl)
+    list(name =fname, 
+         df = res, 
+         nrow = nrow(res), ## data + header+fname
+         ncol = ncol(res))
+}
+
+
+tt1 <- map(l1, lgroup_to_ltables)
+
+library(openxlsx)
+
+sheet_name <- str_glue("diffReps normalization")
+wb <- createWorkbook()
+addWorksheet(wb, sheetName = sheet_name)
+
+cur_row <- 1
+cur_col <- 1
+for(l in tt1){
+    writeData(wb, sheet = sheet_name, l$name, startRow = cur_row, startCol = cur_col, colNames = FALSE)
+    writeData(wb, sheet = sheet_name, l$df, startRow = cur_row+1, startCol = cur_col)
+    cur_row <- 4+cur_row+l$nrow
+}
+saveWorkbook(wb, argv$output, overwrite = T)
+
+
---
+++ Scripts/11_DiffReps_Summary/Run_Jobs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -eu
+module load bedops
+module load R
+module load poppler
+
+source ../00_Setup_Pipeline/01_Pipeline_Setup.sh
+
+rm -rf Job_Summary && mkdir -p Job_Summary
+
+echo "Aggregate pdf files"
+echo
+##### Aggregate pdf files
+#####
+## extract only group names without DIFFREPS and RIPPM suffixes and prefixes
+groups=(`find ../ -maxdepth 1 -type d | grep -v TEMPLATE|grep 10 | sed -E 's/10_diffreps_[1-9]+_//g;s/_RIPPM_w[0-9]+//g;s/_DIFFREPS_w[0-9]+//g' | xargs basename -a | sort | uniq| paste -s -d " "`)
+
+for group in "${groups[@]}"; do
+    echo $group
+    pdfunite `find ../ -name "*.pdf" | grep -v TEMPLATE | grep ${group} | grep -E "FDR"   | paste -s -d " "` "${group}_FDR_Barcharts_MACS2.pdf"
+    pdfunite `find ../ -name "*.pdf" | grep -v TEMPLATE | grep ${group} | grep -E "Hist"  | paste -s -d " "` "${group}_Hist_Barcharts_MACS2.pdf"
+    pdfunite `find ../ -name "*.pdf" | grep -v TEMPLATE | grep ${group} | grep -E "\/Bar" | paste -s -d " "` "${group}_Barcharts_MACS2.pdf"
+
+    mkdir -p "Job_Summary/${group}_PDFs" && mv *.pdf "Job_Summary/${group}_PDFs"
+done
+
+
+echo "Create diffReps track lines"
+echo
+#### convert bed to bb and create tracks
+function diffreps_bed_to_bb2(){
+    local bed_path=$1
+    local dataset_label=$2
+    local bed=`basename $1`
+        
+    bb_name=`echo $bed | sed -e 's/UCSC_FILTERED_track_//g;s/\.bed//g'`
+
+    ## swap colors + ignore chrM and header
+    # cat $bed | sed -E 's/0,0,255/tmp/g;s/255,0,0/0,0,255/g;s/tmp/255,0,0/g' | grep -vE "track|chrM" > tmp1
+
+    # ignore chrM and header
+    cat ${bed_path} | grep -vE "track|chrM" > tmp1
+    sort-bed tmp1 > tmp2 && rm tmp1
+    ./Job_Scripts/bedToBigBed -allow1bpOverlap tmp2 ./Job_Scripts/mm9.chrom.sizes "${bb_name}.bb" && rm tmp2
+
+    track_line="track type=bigBed name=${bb_name} description=${bb_name} visibility=dense itemRgb=on bigDataUrl=http://waxmanlabvm.bu.edu/mpyatkov/${dataset_label}/${bb_name}.bb"
+    echo "${track_line}" > "${bb_name}.bb.ucsc"
+}
+
+for f in `find ../ -name  "*.bed" | grep 10_ | grep _FILTERED`; do
+    echo "Processing: $f"
+    diffreps_bed_to_bb2 $f ${Dataset_Label}
+done
+
+cp *.bb ${VM_DIR_UCSC}
+cat *.ucsc >  ./Job_Summary/all_diffreps_ucsc_tracks.txt
+rm -rf *.bb *.ucsc
+
+echo "Create aggregated xls files"
+echo
+#### create aggregated xls
+Rscript ./Job_Scripts/diffreps_output_parser.R --path ../
+tree
+mv *.xlsx ./Job_Summary
+
+
